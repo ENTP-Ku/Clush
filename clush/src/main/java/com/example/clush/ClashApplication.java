1a190a90032cddafb8411e23f376696f1e7861cd
GuJauk <wkdnr1254@gmail.com>
2025-02-10 09:14:26 +0900

    스프링부트 메이븐 생성

---
+++ clush/src/main/java/com/example/clush/ClashApplication.java
@@ -0,0 +1,13 @@
+package com.example.clush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClashApplication.class, args);
+	}
+
+}
